3f2ccd9134c9136e388cd95a9bef2c8e1a06fdbf
gica <gica4343@tutanota.com>
2025-09-25 21:00:25 +0300

    auto pair

--- init.lua
+++ init.lua
@@ -3,6 +3,14 @@ vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.g.mapleader = " "
 
+vim.keymap.set("i", "(", "()<Left>")
+vim.keymap.set("i", "{", "{}<Left>")
+vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>O")
+vim.keymap.set("i", "[", "[]<Left>")
+vim.keymap.set("i", "<", "<><Left>")
+vim.keymap.set("i", "'", "''<Left>")
+vim.keymap.set("i", "\"", "\"\"<Left>")
+
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.cmd(":hi statusline guibg=NONE")
