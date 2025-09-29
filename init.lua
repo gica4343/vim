7ef8138db1a55105146ba56c4c1e7301a533d324
gica <gica4343@tutanota.com>
2025-09-29 20:36:43 +0300

    lsp

--- init.lua
+++ init.lua
@@ -14,5 +14,36 @@ vim.keymap.set("i", "'", "''<Left>")
 vim.keymap.set("i", "\"", "\"\"<Left>")
 
 vim.keymap.set('n', '<C-s>', ':write<CR>')
-vim.keymap.set('n', '<C-q>', ':quit<CR>')
+vim.keymap.set('n', '<C-c>', ':quit<CR>')
+
+
+vim.pack.add({
+	{src = 'https://github.com/blazkowolf/gruber-darker.nvim', name = 'gruber-darker'},
+	{src = 'https://github.com/neovim/nvim-lspconfig'},
+	{src = 'https://github.com/williamboman/mason.nvim', name = 'mason' },
+    {src = 'https://github.com/williamboman/mason-lspconfig.nvim', name = 'mason-lspconfig' },
+})
+
+require("mason").setup()
+
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "lua_ls",
+        "clangd",
+    },
+    automatic_installation = true,
+})
+
+require("gruber-darker").setup({
+    bold = true,
+    italic = {
+		strings = false,
+        comments = false,
+		operators = false,
+		folds = false,
+    },
+})
+
+vim.cmd.colorscheme("gruber-darker")
+
 vim.cmd(":hi statusline guibg=NONE")
